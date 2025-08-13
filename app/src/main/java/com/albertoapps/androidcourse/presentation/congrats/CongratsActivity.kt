ca5cb655fd51324d9ddf08b9532c998e6c3dcf5b
alberto_bastida <albertobastidapacheco@gmail.com>
2025-08-12 18:35:35 -0600

    fix changes

---
+++ app/src/main/java/com/albertoapps/androidcourse/presentation/congrats/CongratsActivity.kt
@@ -0,0 +1,15 @@
+package com.albertoapps.androidcourse.presentation.congrats
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.albertoapps.androidcourse.R
+
+class CongratsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
